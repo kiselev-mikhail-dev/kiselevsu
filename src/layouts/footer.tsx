165ebfde3816c0b29649a05e4b4650bca3671abc
Mikhail Kiselev <kiselev@simbirsk.ru>
2026-09-14 15:01:22 +0300

    link to github

--- src/layouts/footer.tsx
+++ src/layouts/footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
   return <footer>
     <p className="text-center text-muted">© Киселев М.А., {new Date().getFullYear()}. Вcе права защищены.</p>
-    <p className="text-center text-muted">Исходный код этого сайта <a title="Последних изменений нет, т.к. нет доступа к аккаунту" rel="nofollow" href="https://github.com/mikhail-a-kiselev/kiselevsu">доступен на GitHub</a>.</p>
+    <p className="text-center text-muted">Исходный код этого сайта <a title="gitHub" rel="nofollow" href="https://github.com/kiselev-mikhail-dev/kiselevsu">доступен на GitHub</a>.</p>
   </footer>
 }
 
